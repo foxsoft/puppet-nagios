579bfd4d53253673a3908dc7a1a284581c241b33
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-08-24 10:29:44 +0100

    Workaround for transaction check error

--- manifests/classes/nagios-centos.pp
+++ manifests/classes/nagios-centos.pp
@@ -1,6 +1,5 @@
 class nagios::centos {
-  package {"nagios": ensure => installed}
-  package {"nagios-plugins": ensure => installed, require => Package["nagios"]}
-  package {"nagios-plugins-nrpe": ensure => installed, require => Package["nagios"]}
-  package {"nagios-devel": ensure => installed, require => Package["nagios"]}
+  # This is done due to a transaction check error problem when installing
+  # packages individually (nagios and nagios-devel use some shitty perl file)
+  exec {"yum install -y nagios nagios-plugins nagios-plugins-nrpe nagios-devel": unless => Package["nagios"]}
 }
